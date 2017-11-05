ca6400dc443818d05dc09780156953ea2f845fbf
dabaez02 <dabaez02@hotmail.com>
2017-11-04 20:30:06 -0400

    Changed the order of Title, description and published to Title, published and then description

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 let changeClass = document.getElementById('resizableColumn');
 let amountDonate = document.getElementsByClassName('text_amountdonation')[0];
 let totalAmount = document.getElementById('amountTotal');
+// DONATIONS.HTML RIGHT HANDED INFO COLUMN
 function resizeClass(){changeClass.className = (changeClass.className == "donationsinfocolumn") ? "donationsinfocolumnextended" : "donationsinfocolumn"}
+// DONATIONS.HTML LEFT HANDED INPUT COLUMN
 function donationsValue(donation){console.log(donation);amountDonate.value=donation;totalAmount.innerHTML=donation;}
